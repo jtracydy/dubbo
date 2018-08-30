94d2adf1e765dcfd0668a4bd83032edb7d6107bf
wangdan <jtracydy@163.com>
2018-08-30 20:19:40 +0800

    test commit

--- dubbo-test/src/main/java/test/Demo07.java
+++ dubbo-test/src/main/java/test/Demo07.java
@@ -4,5 +4,11 @@ public class Demo07 {
 
     public static void main(String[] args) {
         System.out.println("adfadfafd");
+
+
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
     }
 }
