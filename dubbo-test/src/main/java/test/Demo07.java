aff8a70b718591659064c5c805af3d83a1b47018
wangdan <jtracydy@163.com>
2018-08-30 20:16:49 +0800

    test

--- dubbo-test/src/main/java/test/Demo07.java
+++ dubbo-test/src/main/java/test/Demo07.java
@@ -4,11 +4,5 @@ public class Demo07 {
 
     public static void main(String[] args) {
         System.out.println("adfadfafd");
-
-
-        System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
-        System.out.println("4");
     }
 }
